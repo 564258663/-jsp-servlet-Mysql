f6bd207e2e9985e030f3ac3635c33eb3e3591d94
564258663 <51950681+564258663@users.noreply.github.com>
2022-07-02 14:35:36 +0800

    Update DBUtil.java

--- student/src/com/demo/util/DBUtil.java
+++ student/src/com/demo/util/DBUtil.java
@@ -9,9 +9,9 @@ import java.sql.SQLException;
  */
 public class DBUtil {
 	private static final String driver = "com.mysql.jdbc.Driver";
-	private static final String url = "jdbc:mysql://hanyy1022.mysql.rds.aliyuncs.com:3306/hanyy_zy?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC";
-	private static final String username = "hanyy_zy";
-	private static final String password = "Hanyy_1022";
+	private static final String url = "jdbc:mysql://xxxxxx:3306/test?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC";
+	private static final String username = "root";
+	private static final String password = "123456";
 
 	private static Connection conn;
 
